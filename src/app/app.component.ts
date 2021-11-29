514f81ded65a7eee7444e53a2678dc4c64baca1e
Ankit <ankit.kumar637849@gmail.com>
2021-11-29 18:39:23 +0530

    my-file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-file';
+  title = 'Pipes tutorial';
+  loginForm= new FormGroup({
+    user:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
+
+  })
+  login()
+  {
+   console.warn(this.loginForm.value)
+  }
+  get user()
+  {
+    return this.loginForm.get('user')
+  }
+  get  password()
+  {
+    return this.loginForm.get('password')
+  }
 }
+   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
-  imports: [
+  imports: [   
     BrowserModule
+    ,FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
